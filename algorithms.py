3197ad913d74304859cef05ba908ee8016b9a54d
Andrea Piras <andrea.piras.3@gmail.com>
2023-11-18 13:13:44 +0100

    Testing A-star and computing distances

---
+++ algorithms.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+from collections import deque
+from queue import PriorityQueue
+from utils import get_valid_moves
+from typing import Tuple, List
+import matplotlib.pyplot as plt
+
+
+def a_star(game : np.ndarray, game_map: np.ndarray, start: Tuple[int, int], target: Tuple[int, int], h: callable) -> List[Tuple[int, int]]:
+    # initialize open and close list
+    open_list = PriorityQueue()
+    close_list = []
+    # additional dict which maintains the nodes in the open list for an easier access and check
+    support_list = {}
+
+    starting_state_g = 0
+    starting_state_h = h(start, target)
+    starting_state_f = starting_state_g + starting_state_h
+
+    open_list.put((starting_state_f, (start, starting_state_g)))
+    support_list[start] = starting_state_g
+    parent = {start: None}
+
+    while not open_list.empty():
+        # get the node with lowest f
+        _, (current, current_cost) = open_list.get()
+        # add the node to the close list
+        close_list.append(current)
+
+        if current == target:
+            print("Target found!")
+            path = build_path(parent, target, game_map, game)
+            print_gamestate(game)
+
+            return path
+
+        for neighbor in get_valid_moves(game_map, current):
+            # check if neighbor in close list, if so continue
+            if neighbor in close_list:
+                continue
+            # compute neighbor g, h and f values
+            neighbor_g = 1 + current_cost
+            neighbor_h = h(neighbor, target)
+            neighbor_f = neighbor_g + neighbor_h
+            parent[neighbor] = current
+            neighbor_entry = (neighbor_f, (neighbor, neighbor_g))
+            # if neighbor in open_list
+            if neighbor in support_list.keys():
+                # if neighbor_g is greater or equal to the one in the open list, continue
+                if neighbor_g >= support_list[neighbor]:
+                    continue
+            
+            # add neighbor to open list and update support_list
+            open_list.put(neighbor_entry)
+            support_list[neighbor] = neighbor_g
+
+    print("Target node not found!")
+    return None
+
+
+def build_path(parent: dict, target: Tuple[int, int], game_map: np.ndarray, game : np.ndarray) -> List[Tuple[int, int]]:
+    path = []
+    while target is not None:
+        path.append(target)
+        agent = get_player_location(game_map)
+        target = parent[target]
+    path.reverse()
+    return path
+
+def print_gamestate(state):
+    plt.imshow(state[100:250, 400:750]) #Immagine ristretta con range [y][x]
+
+
+def get_player_location(game_map: np.ndarray, symbol : str = "@") -> Tuple[int, int]:
+    x, y = np.where(game_map == ord(symbol))
+    return (x[0], y[0])
