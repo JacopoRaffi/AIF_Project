58a3ae1e145b9ec280e0747c2af9603e0cac6aa2
JacopoRaffi <raffijacopo@gmail.com>
2023-11-20 10:23:22 +0100

    map step to action to perform

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import math
 
 from typing import Tuple, List
 
-def get_player_location(game_map: np.ndarray, symbol : str = "@") -> Tuple[int, int]:
+def get_player_location(game_map: np.ndarray, symbol: str = "@") -> Tuple[int, int]:
     """
         gets the coordinates of the player in the game map
         :param game_map: the game map
@@ -53,15 +53,57 @@ def get_valid_moves(game_map: np.ndarray, current_position: Tuple[int, int]) -> 
     # South-East valid move
     if x + 1 < x_limit and y + 1 < y_limit and not is_obstacle(game_map[x+1, y+1]):
         valid.append((x+1, y+1))
-    # North-West valid move
-    if x - 1 > 0 and y - 1 > 0 and not is_obstacle(game_map[x-1, y-1]):
-        valid.append((x-1, y-1))
     # South-West valid move
     if x - 1 > 0 and y + 1 < y_limit and not is_obstacle(game_map[x-1, y+1]):
         valid.append((x-1, y+1))
+    # North-West valid move
+    if x - 1 > 0 and y - 1 > 0 and not is_obstacle(game_map[x-1, y-1]):
+        valid.append((x-1, y-1))
 
     return valid
 
+def action_map(current_position: Tuple[int, int], new_position: Tuple[int, int]) -> int:
+    """
+        get the action to get to the new position from the current one
+        :param new_position: the new coordinates of the agent
+        :param current_position: current coordinates of the agent 
+        :return: the action to get to the new position
+    """
+
+    action = -1
+    x, y = current_position[0], current_position[1]
+    x_new, y_new = new_position[0], new_position[1]
+    action_map = {
+        "N": 0,
+        "E": 1,
+        "S": 2,
+        "W": 3,
+        "NE": 4,
+        "SE": 5,
+        "SW": 6,
+        "NW": 7
+    }
+    if x_new == x:
+        if y_new > y:
+            action = action_map["N"]
+        else: action = action_map["S"]
+    elif y_new == y:
+        if x_new > x:
+            action = action_map["E"]
+        else: action = action_map["W"]
+    elif x_new < x:
+        if y_new > y:
+            action = action_map["NW"]
+        else: action = action_map["SW"]
+    elif x_new > x:
+        if y_new > y:
+            action = action_map["NE"]
+        else: action = action_map["SE"]
+
+    return action
+
+
+#TODO: exploit the action_map function
 def actions_from_path(start: Tuple[int, int], path: List[Tuple[int, int]]) -> List[int]:
     """
         gets all the actions the player has to make to follow a path
